2900c3a4b62927581a487bf0829c54b3b2547a38
clo2go@gmail.com <clo2go@gmail.com>
2016-12-11 00:02:28 -0600

    shutting it down for the night

--- models/Comment.js
+++ models/Comment.js
@@ -1,17 +1,12 @@
-var mongoose = require("mongoose");
+var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var CommentSchema = new Schema({
-
-  // title: {
-  //   type: String
-  // },
   body: {
     type: String
   }
 });
 
-
-var Comment = mongoose.model("Comment", CommentSchema);
+var Comment = mongoose.model('Comment', CommentSchema);
 
 module.exports = Comment;
