f7a6aed1d365b8e8f63fa13820aede54dff0b96f
clo2go <clo2go@gmail.com>
2017-02-02 12:25:27 -0600

    corrected server and mongodb

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
 var exphbs = require('express-handlebars');
 var bodyParser = require('body-parser');
 var request = require('request');
+var logger = require('morgan');
+// Require request and cheerio. This makes the scraping possible
+var request = require('request');
+var cheerio = require('cheerio');
+// Mongoose mpromise deprecated - use bluebird promises
+var Promise = require('bluebird');
 
+mongoose.Promise = Promise;
+
+//Initialize Express
+var app = express();
+
+//body-parse standard code
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
@@ -13,8 +24,14 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+// Set the app up with morgan, body-parser, and a static folder
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static('public'));
 
+//Initialize HDBS engine
 app.engine('handlebars', exphbs({
   defaultLayout:'main'
 }));
@@ -23,25 +40,29 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/controller.js');
 app.use('/', routes);
 
-// mongoose.connect("mongodb://localhost/week18scrape");
-// const db = mongoose.connection
-// 	.on("error", (error) => console.warn("Warning:", error))
-// 	.once("open", () => console.log("Mongoose is walking (they don't run)"));
+var databaseUri = 'mongodb://localhost/scraper';
 
+// Database configuration with mongoose
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI)
+} else {
+	mongoose.connect('databaseUri')
+}
 
+var db = mongoose.connection
 
-// mongoose.connect("mongodb://heroku_pfkzfmn9:uc6mpcpsbd8ad1vmqorj4g35eb@ds127948.mlab.com:27948/heroku_pfkzfmn9");
-// var db = mongoose.connection;
+// Show any mongoose errors
+db.on('error', function(error) {
+  console.log('Mongoose Dont give an F: ', error);
+});
 
-// db.on('error', function(err) {
-//   console.log('Mongoose Error: ', err);
-// });
+// Once logged in to the db through mongoose, log a success message
+db.once('open', function() {
+  console.log('Mongoose finally gave an F.');
+});
 
-// db.once('open', function() {
-//   console.log('Mongoose connection successful.');
-// });
 
 var PORT = process.env.PORT || 3000 ;
 app.listen(PORT, function() {
-  console.log('App running on port: ' + PORT);
+  console.log('App going HARD AF on: ' + PORT);
 });
